64bf11e9aab8b5e831206f7b6286ffe3ee9969f7
touza <touzaelhassan@gmail.com>
2022-04-02 14:36:25 +0000

    Creating project directories and files

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="footer__content">
+    <p>Copyright &copy; 2022 all right reserved.</p>
+  </div>
+</footer>
+<script src="js/main.js"></script>
+</body>
+
+</html>
