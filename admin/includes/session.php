776fe0d4b83bd155a558bdbf71955ee44927a35f
touza <touzaelhassan@gmail.com>
2022-04-03 13:11:00 +0000

    Creating login and logout methods

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -3,11 +3,32 @@
 class Session
 {
   public $user_id;
-  private $signed_in;
+  private $signed_in = false;
 
   function __construct()
   {
     session_start();
+    $this->check_login();
+  }
+
+  public function is_signed_in()
+  {
+    return $this->signed_in;
+  }
+
+  public function login($user)
+  {
+    if ($user) {
+      $this->user_id = $_SESSION["user_id"] = $user->user_id;
+      $this->signed_in = true;
+    }
+  }
+
+  public function logout()
+  {
+    unset($this->user_id);
+    unset($_SESSION["user_id"]);
+    $this->signed_in = false;
   }
 
   private function check_login()
