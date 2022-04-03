3fc2715ce2ff2fb8a03f4b2f60e555d5f44293a0
touza <touzaelhassan@gmail.com>
2022-04-03 12:57:32 +0000

    checking login method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -4,9 +4,21 @@ class Session
 {
   public $user_id;
   private $signed_in;
+
   function __construct()
   {
     session_start();
+  }
+
+  private function check_login()
+  {
+    if (isset($_SESSION["user_id"])) {
+      $this->user_id = $_SESSION["user_id"];
+      $this->signed_in  = true;
+    } else {
+      unset($this->user_id);
+      $this->signed_in = false;
+    }
   }
 }
 
