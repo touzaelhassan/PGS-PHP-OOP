69f97e2b318815917e6c6a5123dcf39e96b3ab6c
touza <touzaelhassan@gmail.com>
2022-05-02 21:42:31 +0000

    Creating self instantiation comment method

--- admin/classes/comment.php
+++ admin/classes/comment.php
@@ -6,4 +6,17 @@ class Comment
   public $photo_id;
   public $comment_author;
   public $comment_body;
+
+  public static function instantiation_comment($photo_id, $comment_author, $comment_body)
+  {
+    if (!empty($photo_id) && !empty($comment_author) && !empty($comment_body)) {
+      $comment = new Comment();
+      $comment->photo_id = $photo_id;
+      $comment->comment_author = $comment_author;
+      $comment->comment_body = $comment_body;
+      return $comment;
+    } else {
+      return false;
+    }
+  }
 }
